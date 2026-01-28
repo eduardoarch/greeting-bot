4d98ac1612d563c21fe4b3234139ca76552d8b39
eduardoarch <eduardobckgnome@gmail.com>
2026-01-28 18:54:45 -0300

    greeting bot

---
+++ greeting bot.js
@@ -0,0 +1,29 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName = "Elonbot";
+let botLocation = "marte";
+let favoriteLanguage = "javascript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " it̛s not really “Java”";
+console.log(codingFact);
+
+codingFact = "in " + favoriteLanguage + ", 0.1 + 0.2 !== 0.3";
+console.log(codingFact);
+
+codingFact =
+  "In " +
+  favoriteLanguage +
+  ", an array can have holes (empty slots) and its length still counts those spaces";
+console.log(codingFact);
+
+console.log(
+  "It was fun sharing these facts with you. Goodbye! - " +
+    botName +
+    " from " +
+    botLocation +
+    ".",
+);
